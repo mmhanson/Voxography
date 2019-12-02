2a7335f9a530fb8e98f5583230d44de35eaf10c0
max <29157840+mmhanson@users.noreply.github.com>
2019-12-02 14:28:59 -0700

    opened a blank window that closes when escape is pressed

--- src/main.c
+++ src/main.c
@@ -4,11 +4,32 @@
 
 int main()
 {
+    GLFWwindow* window;
+
     if (!glfwInit())
     {
         fprintf(stderr, "Failed to initialize glfw.\n");
         return -1;
     }
 
+    // minimum versions
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 
+    window = glfwCreateWindow(640, 480, "My Window", 0, 0);
+    if (!window)
+    {
+        // window creation failed
+    }
+    glfwMakeContextCurrent(window);
+
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+    do
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
+           glfwWindowShouldClose(window) == 0);
 }
