6a56a58dcc120156e1ba4736d828f2e31e478633
max <29157840+mmhanson@users.noreply.github.com>
2019-12-02 20:37:38 -0700

    drew triangle on window. Currently doesn't show anything. Hoping adding shaders make something appear.

--- src/main.c
+++ src/main.c
@@ -20,7 +20,7 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 
     // create window
-    window = glfwCreateWindow(640, 480, "My Window", 0, 0);
+    window = glfwCreateWindow(1024, 768, "My Window", 0, 0);
     if (!window)
     {
         fprintf(stderr, "Failed to create window.\n");
@@ -35,14 +35,46 @@ int main()
         return -1;
     }
 
+    // === triangle stuff ===
     // create triangle VAO (vtx array obj)
     glGenVertexArrays(1, &VertexArrayID);
     glBindVertexArray(VertexArrayID);
+    static const GLfloat g_vertex_buffer_data[] = {
+        -1.0f, -1.0f, 0.0f,
+        1.0f, -1.0f, 0.0f,
+        0.0f,  1.0f, 0.0f,
+    };
+    // This will identify our vertex buffer
+    GLuint vertexbuffer;
+    // Generate 1 buffer, put the resulting identifier in vertexbuffer
+    glGenBuffers(1, &vertexbuffer);
+    // The following commands will talk about our 'vertexbuffer' buffer
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    // Give our vertices to OpenGL.
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
 
     // gameloop
     glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
     do
     {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // === draw triangle === 
+        // 1st attribute buffer : vertices
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+        glVertexAttribPointer(
+            0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+            3,                  // size
+            GL_FLOAT,           // type
+            GL_FALSE,           // normalized?
+            0,                  // stride
+            (void*)0            // array buffer offset
+            );
+        // Draw the triangle !
+        glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
+        glDisableVertexAttribArray(0);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
