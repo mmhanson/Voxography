45c95db31b0ff24911d76943283d74eb21a1f5a8
max <29157840+mmhanson@users.noreply.github.com>
2019-12-05 13:03:54 -0700

    Colored the cube vertices with interpolation between the vertices.

--- shaders/fragment_shader.glsl
+++ shaders/fragment_shader.glsl
@@ -1,7 +1,14 @@
 #version 330 core
 out vec3 color;
+// interpolated values from vertex shader
+in vec3 fragmentColor;
 
 void main()
 {
-    color = vec3(1,0,0);
+    // red
+    //color = vec3(1,0,0);
+
+    // output color->color specified in vtx shader interpolated
+    // btwn 3 surrounding vtxs
+    color = fragmentColor;
 }
--- shaders/vertex_shader.glsl
+++ shaders/vertex_shader.glsl
@@ -1,6 +1,9 @@
 #version 330 core
 //layout(location=0) in vec3 vertexPosition_modelspace;
 layout(location=0) in vec4 position;
+layout(location = 1) in vec3 vertexColor;
+
+out vec3 fragmentColor;
 
 uniform mat4 MVP;
 
@@ -8,4 +11,8 @@ void main()
 {
     //gl_Position = position;
     gl_Position = MVP * position;
+
+    // color for ea vtx will be interpolated to prodcue the color
+    // for each fragment
+    fragmentColor = vertexColor ;
 }
